b42aa8eaa251a0e767430c317b7b12d097c4f2c2
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-19 23:17:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+let container = document.querySelector(".container");
+
+for(let i=1; i<=800; i++) {
+	let box = document.createElement("div");
+	box.className = "box";
+	container.appendChild(box);
+}
+
+let boxes = document.querySelectorAll(".box");
+
+for (let box of boxes) {
+	box.addEventListener("mouseover", getColor);
+	box.addEventListener("mouseout", getDefaultColor);
+}
+
+function getColor(e) {
+	let selectedBox = e.target;
+	selectedBox.style.backgroundColor = randomColor();
+}
+
+function getDefaultColor(e) {
+	let selectedBox = e.target;
+	setTimeout(()=>{
+		selectedBox.style.backgroundColor = "#1d1d1d";
+	},1000)
+}
+
+function randomColor() {
+	let r = Math.floor(Math.random() * 256);
+	let g = Math.floor(Math.random() * 256);
+	let b = Math.floor(Math.random() * 256);
+
+	return `rgb(${r}, ${g}, ${b})`;
+}
+
+
